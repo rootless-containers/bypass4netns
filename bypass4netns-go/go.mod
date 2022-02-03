aa49982b965b1d8fb07afe15338241c684ce2d86
Naoki MATSUMOTO <naoki@pibvt.net>
2022-02-03 08:00:59 +0000

    handle connect(2)
    Signed-off-by: Naoki MATSUMOTO <naoki@pibvt.net>

--- bypass4netns-go/go.mod
+++ bypass4netns-go/go.mod
@@ -9,3 +9,8 @@ require (
 	github.com/sirupsen/logrus v1.8.2-0.20220112234510-85981c045988
 	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27
 )
+
+require (
+	github.com/oraoto/go-pidfd v0.1.2-0.20210402155345-46bf1ba22e22 // indirect
+	github.com/vtolstov/go-ioctl v0.0.0-20151206205506-6be9cced4810 // indirect
+)
--- bypass4netns-go/main.go
+++ bypass4netns-go/main.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -13,13 +14,26 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"syscall"
+	"unsafe"
 
-	securejoin "github.com/cyphar/filepath-securejoin"
 	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/oraoto/go-pidfd"
 	libseccomp "github.com/seccomp/libseccomp-golang"
 	"github.com/sirupsen/logrus"
+	"github.com/vtolstov/go-ioctl"
 	"golang.org/x/sys/unix"
 )
+
+/*
+#include <linux/types.h>
+#include <seccomp.h>
+
+int get_size_of_seccomp_notif_addfd() {
+	return sizeof(struct seccomp_notif_addfd);
+}
+*/
+import "C"
 
 var (
 	socketFile string
@@ -30,6 +44,17 @@ func closeStateFds(recvFds []int) {
 	for i := range recvFds {
 		unix.Close(i)
 	}
+}
+
+func seccomp_iow(nr, typ uintptr) uintptr {
+	return ioctl.IOW(uintptr(C.SECCOMP_IOC_MAGIC), nr, typ)
+}
+
+// C.SECCOMP_IOCTL_NOTIF_ADDFD become error
+// Error Message: could not determine kind of name for C.SECCOMP_IOCTL_NOTIF_ADDFD
+// TODO: use C.SECCOMP_IOCTL_NOTIF_ADDFD
+func seccomp_ioctl_notif_addfd() uintptr {
+	return seccomp_iow(3, uintptr(C.get_size_of_seccomp_notif_addfd()))
 }
 
 // parseStateFds returns the seccomp-fd and closes the rest of the fds in recvFds.
@@ -122,42 +147,162 @@ func handleNewMessage(sockfd int) (uintptr, string, error) {
 	return fd, containerProcessState.Metadata, nil
 }
 
-func readArgString(pid uint32, offset int64) (string, error) {
-	buffer := make([]byte, 4096) // PATH_MAX
+// readProcMem read data from memory of specified pid process at the spcified offset.
+func readProcMem(pid uint32, offset uint64, len uint64) ([]byte, error) {
+	buffer := make([]byte, len) // PATH_MAX
 
 	memfd, err := unix.Open(fmt.Sprintf("/proc/%d/mem", pid), unix.O_RDONLY, 0o777)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer unix.Close(memfd)
 
-	_, err = unix.Pread(memfd, buffer, offset)
+	size, err := unix.Pread(memfd, buffer, int64(offset))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	buffer[len(buffer)-1] = 0
-	s := buffer[:bytes.IndexByte(buffer, 0)]
-	return string(s), nil
+	return buffer[:size], nil
 }
 
-func runMkdirForContainer(pid uint32, fileName string, mode uint32, metadata string) error {
-	// We validated before that metadata is not a string that can make
-	// newFile a file in a different location other than root.
-	newFile := fmt.Sprintf("%s-%s", fileName, metadata)
-	root := fmt.Sprintf("/proc/%d/cwd/", pid)
+type Context struct {
+	notifFd libseccomp.ScmpFd
+	req     *libseccomp.ScmpNotifReq
+	resp    *libseccomp.ScmpNotifResp
+}
 
-	if strings.HasPrefix(fileName, "/") {
-		// If it starts with /, use the rootfs as base
-		root = fmt.Sprintf("/proc/%d/root/", pid)
-	}
+type SeccompNotifAddFd struct {
+	id         uint64
+	flags      uint32
+	srcfd      uint32
+	newfd      uint32
+	newfdFlags uint32
+}
 
-	path, err := securejoin.SecureJoin(root, newFile)
+func handleSysConnect(ctx *Context) {
+	ctx.resp.Flags |= C.SECCOMP_USER_NOTIF_FLAG_CONTINUE
+
+	addrlen := ctx.req.Data.Args[2]
+	buf, err := readProcMem(ctx.req.Pid, ctx.req.Data.Args[1], addrlen)
 	if err != nil {
-		return err
+		logrus.Errorf("Error readProcMem pid %v offset 0x%x: %s", ctx.req.Pid, ctx.req.Data.Args[1], err)
+		return
 	}
 
-	return unix.Mkdir(path, mode)
+	addr := syscall.RawSockaddr{}
+	reader := bytes.NewReader(buf)
+	err = binary.Read(reader, binary.LittleEndian, &addr)
+	if err != nil {
+		logrus.Errorf("Error casting byte array to RawSocksddr: %s", err)
+		return
+	}
+
+	if addr.Family != syscall.AF_INET {
+		logrus.Debugf("Not AF_INET addr: %d", addr.Family)
+		return
+	}
+	addrInet := syscall.RawSockaddrInet4{}
+	reader.Seek(0, 0)
+	err = binary.Read(reader, binary.LittleEndian, &addrInet)
+	if err != nil {
+		logrus.Errorf("Error casting byte array to RawSockaddrInet4: %s", err)
+	}
+
+	logrus.Debugf("%v", addrInet)
+	port := ((addrInet.Port & 0xFF) << 8) | (addrInet.Port >> 8)
+	logrus.Infof("connect(pid=%d): sockfd=%d, port=%d, ip=%v", ctx.req.Pid, ctx.req.Data.Args[0], port, addrInet.Addr)
+
+	switch addrInet.Addr[0] {
+	case 127:
+		logrus.Infof("skipping local ip=%v", addrInet.Addr)
+		return
+	case 10:
+		logrus.Infof("skipping (possibly) (`podmain|nerdctl) network create` network ip=%v", addrInet.Addr)
+		return
+		//case 172:
+		//	logrus.Infof("skipping (possibly) (`docker network create` network ip=%v", addrInet.Addr)
+		//	return
+	}
+
+	targetPidfd, err := pidfd.Open(int(ctx.req.Pid), 0)
+	if err != nil {
+		logrus.Errorf("Pidfd Open failed: %s", err)
+		return
+	}
+	defer syscall.Close(int(targetPidfd))
+
+	sockfd, err := targetPidfd.GetFd(int(ctx.req.Data.Args[0]), 0)
+	if err != nil {
+		logrus.Errorf("Pidfd GetFd failed: %s", err)
+		return
+	}
+	defer syscall.Close(sockfd)
+
+	logrus.Debugf("got sockfd=%v", sockfd)
+	sock_domain, err := syscall.GetsockoptInt(sockfd, syscall.SOL_SOCKET, syscall.SO_DOMAIN)
+	if err != nil {
+		logrus.Errorf("getsockopt(SO_DOMAIN) failed: %s", err)
+		return
+	}
+
+	if sock_domain != syscall.AF_INET {
+		logrus.Errorf("expected AF_INET, got %d", sock_domain)
+		return
+	}
+
+	sock_type, err := syscall.GetsockoptInt(sockfd, syscall.SOL_SOCKET, syscall.SO_TYPE)
+	if err != nil {
+		logrus.Errorf("getsockopt(SO_TYPE) failed: %s", err)
+		return
+	}
+
+	sock_protocol, err := syscall.GetsockoptInt(sockfd, syscall.SOL_SOCKET, syscall.SO_PROTOCOL)
+	if err != nil {
+		logrus.Errorf("getsockopt(SO_PROTOCOL) failed: %s", err)
+		return
+	}
+
+	sockfd2, err := syscall.Socket(sock_domain, sock_type, sock_protocol)
+	if err != nil {
+		logrus.Errorf("socket failed: %s", err)
+	}
+	defer syscall.Close(sockfd2)
+
+	addfd := SeccompNotifAddFd{
+		id:         ctx.req.ID,
+		flags:      C.SECCOMP_ADDFD_FLAG_SETFD,
+		srcfd:      uint32(sockfd2),
+		newfd:      uint32(ctx.req.Data.Args[0]),
+		newfdFlags: 0,
+	}
+
+	ioctl_op := seccomp_ioctl_notif_addfd()
+	logrus.Debugf("ioctl_op:%v", ioctl_op)
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(ctx.notifFd), ioctl_op, uintptr(unsafe.Pointer(&addfd)))
+	if errno != 0 {
+		logrus.Errorf("ioctl(SECCOMP_IOCTL_NOTIF_ADFD) failed: %s", err)
+		return
+	}
+}
+
+func handleReq(ctx *Context) {
+	syscallName, err := ctx.req.Data.Syscall.GetName()
+	if err != nil {
+		logrus.Errorf("Error decoding syscall %v(): %s", ctx.req.Data.Syscall, err)
+		// TODO: error handle
+		return
+	}
+	logrus.Debugf("Received syscall %q, pid %v, arch %q, args %+v", syscallName, ctx.req.Pid, ctx.req.Data.Arch, ctx.req.Data.Args)
+
+	switch syscallName {
+	case "connect":
+		handleSysConnect(ctx)
+	default:
+		logrus.Errorf("Unknown syscall %q", syscallName)
+		// TODO: error handle
+		return
+	}
+
 }
 
 // notifHandler handles seccomp notifications and responses
@@ -169,18 +314,16 @@ func notifHandler(fd libseccomp.ScmpFd, metadata string) {
 			logrus.Errorf("Error in NotifReceive(): %s", err)
 			continue
 		}
-		syscallName, err := req.Data.Syscall.GetName()
-		if err != nil {
-			logrus.Errorf("Error decoding syscall %v(): %s", req.Data.Syscall, err)
-			continue
-		}
-		logrus.Debugf("Received syscall %q, pid %v, arch %q, args %+v", syscallName, req.Pid, req.Data.Arch, req.Data.Args)
 
-		resp := &libseccomp.ScmpNotifResp{
-			ID:    req.ID,
-			Error: 0,
-			Val:   0,
-			Flags: libseccomp.NotifRespFlagContinue,
+		ctx := Context{
+			notifFd: fd,
+			req:     req,
+			resp: &libseccomp.ScmpNotifResp{
+				ID:    req.ID,
+				Error: 0,
+				Val:   0,
+				Flags: libseccomp.NotifRespFlagContinue,
+			},
 		}
 
 		// TOCTOU check
@@ -189,38 +332,9 @@ func notifHandler(fd libseccomp.ScmpFd, metadata string) {
 			continue
 		}
 
-		switch syscallName {
-		case "mkdir":
-			fileName, err := readArgString(req.Pid, int64(req.Data.Args[0]))
-			if err != nil {
-				logrus.Errorf("Cannot read argument: %s", err)
-				resp.Error = int32(unix.ENOSYS)
-				resp.Val = ^uint64(0) // -1
-				goto sendResponse
-			}
+		handleReq(&ctx)
 
-			logrus.Debugf("mkdir: %q", fileName)
-
-			// TOCTOU check
-			if err := libseccomp.NotifIDValid(fd, req.ID); err != nil {
-				logrus.Errorf("TOCTOU check failed: req.ID is no longer valid: %s", err)
-				continue
-			}
-
-			err = runMkdirForContainer(req.Pid, fileName, uint32(req.Data.Args[1]), metadata)
-			if err != nil {
-				resp.Error = int32(unix.ENOSYS)
-				resp.Val = ^uint64(0) // -1
-			}
-			resp.Flags = 0
-		case "chmod", "fchmod", "fchmodat":
-			resp.Error = int32(unix.ENOMEDIUM)
-			resp.Val = ^uint64(0) // -1
-			resp.Flags = 0
-		}
-
-	sendResponse:
-		if err = libseccomp.NotifRespond(fd, resp); err != nil {
+		if err = libseccomp.NotifRespond(fd, ctx.resp); err != nil {
 			logrus.Errorf("Error in notification response: %s", err)
 			continue
 		}
@@ -228,7 +342,8 @@ func notifHandler(fd libseccomp.ScmpFd, metadata string) {
 }
 
 func main() {
-	flag.StringVar(&socketFile, "socketfile", "/run/seccomp-agent.socket", "Socket file")
+	xdg_runtime_dir := os.Getenv("XDG_RUNTIME_DIR")
+	flag.StringVar(&socketFile, "socketfile", xdg_runtime_dir+"/bypass4netns.sock", "Socket file")
 	flag.StringVar(&pidFile, "pid-file", "", "Pid file")
 	logrus.SetLevel(logrus.DebugLevel)
 
