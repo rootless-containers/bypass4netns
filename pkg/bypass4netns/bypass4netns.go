24bdeb5a112b47c1b573584cab5a52f6912bf15f
Naoki MATSUMOTO <naoki@pibvt.net>
2022-02-09 16:31:03 +0900

    Revert "add a function to get interface ipv4 addresses from pid"
    This reverts commit 4a0abcdf81b2df2562f69f797e1bee42e712507e.
    
    Signed-off-by: Naoki MATSUMOTO <naoki@pibvt.net>

--- pkg/bypass4netns/bypass4netns.go
+++ pkg/bypass4netns/bypass4netns.go
@@ -10,9 +10,6 @@ import (
 	"errors"
 	"fmt"
 	"net"
-	"os/exec"
-	"strconv"
-	"strings"
 	"syscall"
 
 	"github.com/opencontainers/runtime-spec/specs-go"
@@ -304,56 +301,6 @@ func (h *notifHandler) manageSocket(logPrefix string, destAddr net.IP, pid int, 
 	}
 }
 
-// getInet4AddressesFromPid get interface ipaddress of the netns the pid belonging.
-// loopback address(127.0.0.0/8) is ignored.
-func getInet4AddressesFromPid(pid int) ([]net.IP, error) {
-	res, err := exec.Command("nsenter", "--target", strconv.Itoa(pid), "--user", "--net", "ip", "-o", "addr").Output()
-	if err != nil {
-		return nil, fmt.Errorf("failed to exec `nsenter ip a s` in pid=%d", pid)
-	}
-
-	_, loSubnets, err := net.ParseCIDR("127.0.0.0/8")
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate LoopBack subnet")
-	}
-
-	ips := []net.IP{}
-	lines := strings.Split(string(res), "\n")
-	for _, line := range lines {
-		// Ignore interfaces not having ipv4 address
-		if !strings.Contains(line, "inet") {
-			continue
-		}
-
-		// Ignore ipv6 address
-		if strings.Contains(line, "inet6") {
-			continue
-		}
-
-		// try to parse address from below like outputs
-		// "1: lo    inet 127.0.0.1/8 scope host lo\       valid_lft forever preferred_lft forever"
-		elements := strings.Fields(line)
-		for idx, e := range elements {
-			if e != "inet" {
-				continue
-			}
-			if idx+1 >= len(elements) {
-				break
-			}
-			ipAddrStr := elements[idx+1]
-			ipAddr, _, err := net.ParseCIDR(ipAddrStr)
-			if err != nil {
-				return nil, fmt.Errorf("failed to parse %s: %s", ipAddrStr, err)
-			}
-			if loSubnets.Contains(ipAddr) {
-				break
-			}
-			ips = append(ips, ipAddr)
-		}
-	}
-	return ips, nil
-}
-
 // handleSysBind handles syscall bind(2).
 // If binding port is the target of port-forwarding,
 // it creates and configures including bind(2) a socket on host.
@@ -368,13 +315,6 @@ func (h *notifHandler) handleSysBind(ctx *context) {
 	logrus.Debugf("%v", addrInet)
 	port := ((addrInet.Port & 0xFF) << 8) | (addrInet.Port >> 8)
 	logrus.Infof("bind(pid=%d): sockfd=%d, port=%d, ip=%v", ctx.req.Pid, ctx.req.Data.Args[0], port, addrInet.Addr)
-
-	ips, err := getInet4AddressesFromPid(int(ctx.req.Pid))
-	if err != nil {
-		logrus.Errorf("failed to get ip address from pid=%d: %s", ctx.req.Pid, err)
-		return
-	}
-	logrus.Debugf("target address %v", ips)
 
 	// TODO: get port-fowrad mapping from nerdctl
 	fwdPort, ok := h.forwardingPorts[int(port)]
