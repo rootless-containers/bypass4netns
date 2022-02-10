2fa03f13ab9f045769339e277bdadaa63bf94e37
Naoki MATSUMOTO <naoki@pibvt.net>
2022-02-11 00:19:52 +0900

    move files related to nerdctl integration to nerdctl
    Signed-off-by: Naoki MATSUMOTO <naoki@pibvt.net>

--- pkg/oci/oci.go
+++ pkg/oci/oci.go
@@ -1,13 +1,6 @@
 package oci
 
 import (
-	gocontext "context"
-	"fmt"
-	"os"
-	"path/filepath"
-
-	"github.com/containerd/containerd/containers"
-	"github.com/containerd/containerd/oci"
 	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
@@ -15,7 +8,7 @@ const (
 	SocketName = "bypass4netns.sock"
 )
 
-func getDefaultSeccompProfile(listenerPath string) *specs.LinuxSeccomp {
+func GetDefaultSeccompProfile(listenerPath string) *specs.LinuxSeccomp {
 	seccomp := &specs.LinuxSeccomp{
 		DefaultAction: specs.ActAllow,
 		Architectures: []specs.Arch{specs.ArchX86_64, specs.ArchX86, specs.ArchX32},
@@ -29,63 +22,4 @@ func getDefaultSeccompProfile(listenerPath string) *specs.LinuxSeccomp {
 	}
 
 	return seccomp
-}
-func GenerateSecurityOpt(listenerPath string) (oci.SpecOpts, error) {
-	opt := func(_ gocontext.Context, _ oci.Client, _ *containers.Container, s *specs.Spec) error {
-		s.Linux.Seccomp = getDefaultSeccompProfile(listenerPath)
-		return nil
-	}
-	return opt, nil
-}
-
-func getXDGRuntimeDir() (string, error) {
-	if xrd := os.Getenv("XDG_RUNTIME_DIR"); xrd != "" {
-		return xrd, nil
-	}
-	return "", fmt.Errorf("environment variable XDG_RUNTIME_DIR is not set")
-}
-
-func CreateSocketDir() error {
-	xdgRuntimeDir, err := getXDGRuntimeDir()
-	if err != nil {
-		return err
-	}
-	dirPath := filepath.Join(xdgRuntimeDir, "bypass4netns")
-	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
-		err = os.MkdirAll(dirPath, 0775)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func GetBypass4NetnsdDefaultSocketPath() (string, error) {
-	xdgRuntimeDir, err := getXDGRuntimeDir()
-	if err != nil {
-		return "", err
-	}
-
-	return filepath.Join(xdgRuntimeDir, "bypass4netnsd.sock"), nil
-}
-
-func GetSocketPathByID(id string) (string, error) {
-	xdgRuntimeDir, err := getXDGRuntimeDir()
-	if err != nil {
-		return "", err
-	}
-
-	socketPath := filepath.Join(xdgRuntimeDir, "bypass4netns", id[0:15]+".sock")
-	return socketPath, nil
-}
-
-func GetPidFilePathByID(id string) (string, error) {
-	xdgRuntimeDir, err := getXDGRuntimeDir()
-	if err != nil {
-		return "", err
-	}
-
-	socketPath := filepath.Join(xdgRuntimeDir, "bypass4netns", id[0:15]+".pid")
-	return socketPath, nil
 }
