50d1be474770fab0ed42187bd6ee9a74e4e8e24e
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2022-09-02 14:39:26 +0900

    pkg/oci: refactor
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- pkg/oci/oci.go
+++ pkg/oci/oci.go
@@ -1,6 +1,9 @@
 package oci
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
@@ -8,18 +11,31 @@ const (
 	SocketName = "bypass4netns.sock"
 )
 
-func GetDefaultSeccompProfile(listenerPath string) *specs.LinuxSeccomp {
-	seccomp := &specs.LinuxSeccomp{
-		DefaultAction: specs.ActAllow,
-		Architectures: []specs.Arch{specs.ArchX86_64, specs.ArchX86, specs.ArchX32},
-		ListenerPath:  listenerPath,
-		Syscalls: []specs.LinuxSyscall{
-			{
-				Names:  []string{"bind", "close", "connect", "sendmsg", "sendto", "setsockopt"},
-				Action: specs.ActNotify,
-			},
-		},
-	}
+var SyscallsToBeNotified = []string{"bind", "close", "connect", "sendmsg", "sendto", "setsockopt"}
 
+func GetDefaultSeccompProfile(listenerPath string) *specs.LinuxSeccomp {
+	tmpl := specs.LinuxSeccomp{
+		DefaultAction: specs.ActAllow,
+	}
+	seccomp, err := TranslateSeccompProfile(tmpl, listenerPath)
+	if err != nil {
+		panic(err)
+	}
 	return seccomp
+}
+
+func TranslateSeccompProfile(old specs.LinuxSeccomp, listenerPath string) (*specs.LinuxSeccomp, error) {
+	sc := old
+	if sc.ListenerPath != "" && sc.ListenerPath != listenerPath {
+		return nil, fmt.Errorf("bypass4netns's seccomp listener path %q conflicts with the existing seccomp listener path %q", listenerPath, sc.ListenerPath)
+	}
+	sc.ListenerPath = listenerPath
+	prepend := specs.LinuxSyscall{
+		Names:  SyscallsToBeNotified,
+		Action: specs.ActNotify,
+	}
+	if alreadyPrepended := len(sc.Syscalls) > 0 && reflect.DeepEqual(sc.Syscalls[0], sc.ListenerPath); !alreadyPrepended {
+		sc.Syscalls = append([]specs.LinuxSyscall{prepend}, sc.Syscalls...)
+	}
+	return &sc, nil
 }
