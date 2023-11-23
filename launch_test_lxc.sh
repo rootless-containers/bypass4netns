d9fddceefc538a83502072342a380e31dc8f2258
Naoki MATSUMOTO <naoki@pibvt.net>
2023-11-23 12:16:20 +0000

    retry until success
    Signed-off-by: Naoki MATSUMOTO <naoki@pibvt.net>

--- launch_test_lxc.sh
+++ launch_test_lxc.sh
@@ -7,4 +7,11 @@ sudo lxc launch -c security.nesting=true images:ubuntu/22.04 test
 sudo lxc config device add test share disk source=$(pwd) path=/host
 sudo lxc exec test -- /bin/bash -c "echo 'ubuntu ALL=NOPASSWD: ALL' | EDITOR='tee -a' visudo"
 # let user services running
-sudo lxc exec test -- sudo --login --user ubuntu /bin/bash -c "sleep 3 && sudo loginctl enable-linger"
+# this sometimes fails, retry until success
+RES=1
+while [ $RES -ne 0 ]
+do
+    sleep 1
+    sudo lxc exec test -- sudo --login --user ubuntu /bin/bash -c "sudo loginctl enable-linger"
+    RES=$?
+done
